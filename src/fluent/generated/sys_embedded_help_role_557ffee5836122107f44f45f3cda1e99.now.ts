bb0ba284418a89e21f85402d7c9543a00ef2adea
System Administrator <admin@example.com>
2025-05-16 19:12:28 +0300

    role

---
+++ src/fluent/generated/sys_embedded_help_role_557ffee5836122107f44f45f3cda1e99.now.ts
@@ -0,0 +1,11 @@
+import { Record } from "@servicenow/sdk/core";
+
+export default Record({
+    $id: "557ffee5836122107f44f45f3cda1e99",
+    table: "sys_embedded_help_role",
+    data: {
+        order: 10,
+        role: "a76f32a9836122107f44f45f3cda1e5f",
+        sys_name: "a76f32a9836122107f44f45f3cda1e5f"
+    }
+});
---
+++ src/fluent/generated/sys_user_role_a76f32a9836122107f44f45f3cda1e5f.now.ts
@@ -0,0 +1,10 @@
+import { Role } from "@servicenow/sdk/core";
+
+export default Role({
+    $id: "a76f32a9836122107f44f45f3cda1e5f",
+    can_delegate: true,
+    elevated_privilege: false,
+    grantable: true,
+    name: "x_snc_cust_a_saf.admin",
+    scoped_admin: false
+});
